1a3aa0bec60ae88b3bdae6b5c4ee28fb84190246
mncrmn2009 <mnc.rmn.2009@gmail.com>
2024-11-27 08:37:27 -0300

    Clase Abtracta Vuelo

---
+++ src/Ejercicio1/Vuelos.java
@@ -0,0 +1,48 @@
+package Ejercicio1;
+
+public abstract class Vuelos {
+    protected int numeroVuelo;
+    protected String origen;
+    protected String destino;
+    protected String fecha;
+
+    public Vuelos(int numeroVuelo, String origen, String destino, String fecha) {
+        this.numeroVuelo = numeroVuelo;
+        this.origen = origen;
+        this.destino = destino;
+        this.fecha = fecha;
+    }
+
+    public int getNumeroVuelo() {
+        return numeroVuelo;
+    }
+
+    public void setNumeroVuelo(int numeroVuelo) {
+        this.numeroVuelo = numeroVuelo;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public abstract double calcularPrecio();
+}
